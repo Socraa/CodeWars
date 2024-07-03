beead89f1039b4c45c97fd0f4c472cbe6ac9572f
Jai Dyelle Dela Cruz <Jaidyelle12@gmail.com>
2024-07-03 14:50:37 +0800

    for loop

---
+++ CodeWars-4/script.js
@@ -0,0 +1,17 @@
+function betterThanAverage(classPoints, yourPoints) {
+    let sum = 0
+    
+    for( let i = 0; i < classPoints.length; i++){
+      sum += classPoints[i];
+    }
+    
+    if ((sum / classPoints.length) <= yourPoints){
+        return true;
+    
+    }else{
+      return false;
+    }
+  }
+
+let arr = betterThanAverage([34,2,4,2,3,545,23], 999);
+console.log(arr); // Output: True
